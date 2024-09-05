45aa4476108ae651a4b1448b6cc7c408bf9354ce
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-05 20:06:30 +0600

    concat use

---
+++ concat.js
@@ -0,0 +1,7 @@
+const first = "Rakibul";
+const last = "Hassan";
+const fullName = first + ' ' + last;
+console.log(fullName);
+
+const fullName2 = first.concat(' ').concat(last);
+console.log(fullName2);
