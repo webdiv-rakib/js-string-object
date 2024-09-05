638ae1cfaadbdba73ccd91892352a0391b30497f
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-06 04:53:46 +0600

    object property

---
+++ property.js
@@ -0,0 +1,12 @@
+const person = {
+    name: "Abu Nayim Mohammad Rakib",
+    age: 25,
+    profession: 'Developer',
+    salary: 25000,
+    married: true
+}
+// console.log(person);
+console.log(person.age);
+
+const income = person.salary;
+console.log(income);
