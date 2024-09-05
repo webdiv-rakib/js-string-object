b6424a0a991d688c139ca79b5f984ef6827cbfaa
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-05 20:44:44 +0600

    object define structure

---
+++ object.js
@@ -0,0 +1,34 @@
+// Primitive
+// Basic
+const age = 21;
+const school = 'Bindubasini';
+const isPassed = true;
+// let isDeveloper;
+const subjects = ['Bangla', 'English', 'Math'];
+// console.log(isDeveloper);
+const bottle_wrong = ['white', 45,];
+const bottleColor = 'white';
+const bottlePrice = 45;
+const bottleDrinks = "water";
+
+// object
+// non-primitive
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+// non-primitive
+const subject = {
+    name: 'biology',
+    teacher: "rasheda mam",
+    examDate: '30 Feb',
+    chapters: ['first', 'second', 'third'],
+    exams: {
+        name: 'Final Exam',
+        marks: 100
+    }
+}
+console.log(subject);
