2d98cfbd951df5d69499f431df685c93d047c301
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-05 16:45:04 +0600

    sting vs array

---
+++ string.js
@@ -0,0 +1,21 @@
+const country = 'Bangladesh';
+const division = "Dhaka";
+const district = `Tangail`;
+// const thana = new String('Delduar');
+
+// console.log(typeof country);
+// console.log(typeof thana);
+
+const numbers = [25, 55, 35, 45, 76, 84]
+// console.log(numbers.length);
+// console.log(numbers[0]);
+numbers[1] = 11;
+console.log(numbers);
+
+const capital = 'Dhaka';
+// console.log(capital);
+// console.log(capital.length);
+// console.log(capital[4]);
+console.log(capital);
+capital[0] = 'T';
+console.log(capital);
