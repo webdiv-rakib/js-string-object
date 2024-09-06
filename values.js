e64260287ff118e12389977b894daf28a56b131a
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-06 16:45:56 +0600

    use of values example

---
+++ values.js
@@ -0,0 +1,11 @@
+const computer = {
+    brand: 'Lenovo',
+    price: 35000,
+    processor: 'Intel',
+    gpu: 'GTX 1650 Super',
+    monitor: 'Gigabyte',
+    ssd: '512GB',
+    ram: '16GB',
+}
+const values = Object.values(computer);
+console.log(values);
