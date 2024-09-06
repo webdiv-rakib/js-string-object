f9995112fe0aa35400e35c99e51c6d79bc617c2c
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-06 16:34:11 +0600

    property value update example using dot and bracket notation

---
+++ value.js
@@ -0,0 +1,11 @@
+const information = {
+    name: 'Abu Nayim Mohammad Rakib',
+    age: 26,
+    address: 'Tangail',
+    profession: 'Student',
+    salary: 10000,
+    married: false
+}
+information.salary = 30000;
+information['age'] = 30;
+console.log(information);
