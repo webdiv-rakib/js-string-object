915e5b31a34c6a85b161b17b34b23fab38dc653a
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-06 17:56:57 +0600

    task-3 solve

---
+++ task-3.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+
+console.log(student);
+console.log(student.physics.marks);
