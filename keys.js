68041fed799873b9e55b3fbe839abd7dcafc1c04
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-06 16:42:36 +0600

    keys use example

---
+++ keys.js
@@ -0,0 +1,12 @@
+const computer = {
+    brand: 'Lenovo',
+    price: 35000,
+    processor: 'Intel',
+    gpu: 'GTX 1650 Super',
+    monitor: 'Gigabyte',
+    ssd: '512GB',
+    ram: '16GB',
+}
+
+const keys = Object.keys(computer);
+console.log(keys);
