c010c6a733e756616278a38e823ad25eb23277db
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-06 17:14:55 +0600

    object declare

---
+++ declare.js
@@ -0,0 +1,7 @@
+const pen = { brand: 'econo', price: 15, color: 'balck' }
+const pencel = new Object()
+console.log(pencel);
+const rubber = Object.create({});
+console.log(rubber);
+
+// Using new 
