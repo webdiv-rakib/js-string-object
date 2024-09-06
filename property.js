11a1a14e330ebf779bc0c4cfa741e9e9456a8f1d
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-06 16:28:22 +0600

    use case of dot notation and bracket notation

--- property.js
+++ property.js
@@ -1,12 +1,32 @@
-const person = {
-    name: "Abu Nayim Mohammad Rakib",
-    age: 25,
-    profession: 'Developer',
-    salary: 25000,
-    married: true
-}
+// const person = {
+//     name: "Abu Nayim Mohammad Rakib",
+//     age: 25,
+//     profession: 'Developer',
+//     salary: 25000,
+//     married: true
+// }
 // console.log(person);
-console.log(person.age);
+// console.log(person.age);
 
-const income = person.salary;
-console.log(income);
+// const income = person.salary;
+// console.log(income);
+
+// dot notation
+// dot symbol use kore object er property ber kora
+const information = {
+    name: 'Abu Nayim Mohammad Rakib',
+    age: 26,
+    address: 'Tangail',
+    profession: 'Student',
+    salary: 10000,
+    married: false
+}
+console.log(information.name);
+const name1 = information.name;
+console.log(name1);
+
+// bracket notation
+// third bracker diye access kora
+console.log(information['age']);
+const boyos = information['age'];
+console.log(boyos);
